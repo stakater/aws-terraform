9e350f33a2b1cb9ffeab501b273e81ace9575374
ikramkami30 <ikramulhaq23@yahoo.com>
2016-05-26 10:33:12 +0500

    removed keypair name for snapshot module

--- resources/terraforms/module-snapshot.tf
+++ resources/terraforms/module-snapshot.tf
@@ -1,17 +1,6 @@
 module "snapshot" {
     source = "../modules/snapshot"
-
     keypair = "snapshot"
-
-    # aws
-    aws_account_id="${var.aws_account.id}"
-    aws_region = "${var.aws_account.default_region}"
-    ami = "${var.ami}"
-
-    #cluster name
-    cluster_name = "${var.cluster_name}"
-
-
     instance_id = "${module.amicreation.instance_id}"
     
 }
