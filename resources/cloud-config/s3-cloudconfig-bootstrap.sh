0ff101271ee11bc73cb5ea4158606554f52ec0ff
hazim1093 <hazim_malik@hotmail.com>
2016-06-15 15:48:25 +0500

    Update bootstrap script to download `upload-certificates.sh` file

--- resources/cloud-config/s3-cloudconfig-bootstrap.sh
+++ resources/cloud-config/s3-cloudconfig-bootstrap.sh
@@ -98,6 +98,33 @@ curl -s -L -O -H "Host: ${configBucket}.s3.amazonaws.com" \
   -H "Date: ${dateValue}" \
   https://${configBucket}.s3.amazonaws.com/${logstashConfigFile}
 
+#######################################################################
+# Download `upload-certificates.sh` from admiral_global to /etc/scripts
+#
+# Downloading this script will allow gen-certificate.service to generate
+# certificates then upload them to s3
+#######################################################################
+
+scriptsDir="/etc/scripts"
+mkdir -m 700 -p ${scriptsDir}
+cd ${scriptsDir}
+
+uploadScriptFile="CLUSTER-NAME_admiral_global/upload-certificates.sh"
+
+resource="/${configBucket}/${uploadScriptFile}"
+create_string_to_sign
+signature=$(/bin/echo -n "$stringToSign" | openssl sha1 -hmac ${s3Secret} -binary | base64)
+filePath=${uploadScriptFile}
+debug_log
+curl -s -L -O -H "Host: ${configBucket}.s3.amazonaws.com" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  -H "x-amz-security-token:${s3Token}" \
+  -H "Date: ${dateValue}" \
+  https://${configBucket}.s3.amazonaws.com/${uploadScriptFile}
+
+# make script file executable
+chmod a+x upload-certificates.sh
 
 ########################################################################
 # Download CLUSTER-NAME-cloudinit/<profile>/clould-config.yaml
