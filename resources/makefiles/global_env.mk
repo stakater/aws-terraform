6244dddd00bccf9009d99655a3f31a90c74faadc
hazim1093 <hazim_malik@hotmail.com>
2016-06-21 12:36:46 +0500

    Add GoCD to make sequence

--- resources/makefiles/global_env.mk
+++ resources/makefiles/global_env.mk
@@ -1,5 +1,5 @@
-global_env: docker_registry route53 base_instance
+global_env: docker_registry route53 gocd
 
-destroy_global_env: destroy_base_instance destroy_route53 destroy_docker_registry
+destroy_global_env: destroy_gocd destroy_route53 destroy_docker_registry
 
 .PHONY: global_env destroy_global_env
