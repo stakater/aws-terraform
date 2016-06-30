4e3df426ca8cb842408263680fa95fc953693a14
hazim1093 <hazim_malik@hotmail.com>
2016-06-30 17:35:16 +0500

    Add option for specifying Makefile location

--- scripts/substitute-makefile-vars.sh
+++ scripts/substitute-makefile-vars.sh
@@ -9,15 +9,21 @@
 DOCKER_IMAGE=""
 DOCKER_OPTS=""
 CLUSTER_NAME=""
+MAKEFILE=""
 
 # Flags to make sure all options are given
+mOptionFlag=false;
 iOptionFlag=false;
 oOptionFlag=false;
 cOptionFlag=false;
 # Get options from the command line
-while getopts ":i:o:c" OPTION
+while getopts ":m:i:o:c" OPTION
 do
     case $OPTION in
+        m)
+          mOptionFlag=true
+          MAKEFILE=$OPTARG
+          ;;
         i)
           iOptionFlag=true
           DOCKER_IMAGE=$OPTARG
@@ -31,20 +37,20 @@ do
           CLUSTER_NAME=$OPTARG
           ;;
         *)
-          echo "Usage: $(basename $0) -i <Base App Docker Image> -o <Docker Options> -c <Cluster Name>"
+          echo "Usage: $(basename $0) -m <Makefile location> -i <Base App Docker Image> -o <Docker Options> -c <Cluster Name>"
           exit 0
           ;;
     esac
 done
 
-if ! $iOptionFlag || ! $oOptionFlag;
+if !$mOptionFlag || ! $iOptionFlag || ! $oOptionFlag || ! $cOptionFlag;
 then
-  echo "Usage: $(basename $0) -i <Base App Docker Image> -o <Docker Options> -c <Cluster Name>"
+  echo "Usage: $(basename $0) -m <Makefile location> -i <Base App Docker Image> -o <Docker Options> -c <Cluster Name>"
   exit 0;
 fi
 
 # substitue whole line where string 'BASE_APP_DOCKER_IMG' appears
 makeFile="../Makefile"
-sed -i "/BASE_APP_DOCKER_IMG/c\BASE_APP_DOCKER_IMG := \\${DOCKER_IMAGE}" $makeFile
-sed -i "/BASE_APP_DOCKER_OPTS/c\BASE_APP_DOCKER_OPTS := \\${DOCKER_OPTS}" $makeFile
-sed -i "/CLUSTER_NAME/c\CLUSTER_NAME := \\${CLUSTER_NAME}" $makeFile
+sed -i "/BASE_APP_DOCKER_IMG/c\BASE_APP_DOCKER_IMG := \\${DOCKER_IMAGE}" $MAKEFILE
+sed -i "/BASE_APP_DOCKER_OPTS/c\BASE_APP_DOCKER_OPTS := \\${DOCKER_OPTS}" $MAKEFILE
+sed -i "/CLUSTER_NAME/c\CLUSTER_NAME := \\${CLUSTER_NAME}" $MAKEFILE
