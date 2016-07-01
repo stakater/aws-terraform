efa757048bd5643929828218aa6dc90d5df2d3e4
hazim1093 <hazim_malik@hotmail.com>
2016-07-01 17:35:10 +0500

    WIP

--- scripts/substitute-app-docker-vars.sh
+++ scripts/substitute-app-docker-vars.sh
@@ -2,19 +2,18 @@
 
 # Replace DOCKER_IMAGE and DOCKER_OPTS with user defined values.
 
-DOCKER_IMAGE=${APP_DOCKER_IMAGE//////}
+DOCKER_IMAGE=${APP_DOCKER_IMAGE}
 DOCKER_OPTS=${APP_DOCKER_OPTS}
 
 echo "$DOCKER_IMAGE";
-echo "\"$DOCKER_OPTS\"";
+echo "$DOCKER_OPTS";
 
 files=$(grep -s -l -e \<#DOCKER_IMAGE#\> -e \<#DOCKER_OPTS#\> -r $@)
-echo "${files[@]}"
 if [ "X$files" != "X" ];
 then
   for f in $files
   do
-    perl -p -i -e "s/<#DOCKER_IMAGE#>/\"$DOCKER_IMAGE\"/g" $f
-    #perl -p -i -e "s/<#DOCKER_OPTS#>/\"$DOCKER_OPTS\"/g" $f
+    perl -p -i -e "s|<#DOCKER_IMAGE#>|$DOCKER_IMAGE|g" $f
+    perl -p -i -e "s|<#DOCKER_OPTS#>|$DOCKER_OPTS|g" $f
   done
 fi
