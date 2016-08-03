51ce6d6533cd80786c5e9a326ab6899332184fa8
Hazim <hazim@aurorasolutions.io>
2016-08-03 22:23:26 +0500

    Add worker_dev and worker_qa

--- resources/makefiles/global_env.mk
+++ resources/makefiles/global_env.mk
@@ -1,5 +1,5 @@
-global_env: docker_registry route53 gocd
+global_env: docker_registry route53 gocd worker_dev worker_qa
 
-destroy_global_env: destroy_gocd destroy_route53 destroy_docker_registry
+destroy_global_env: destroy_worker_dev destroy_worker_qa destroy_gocd destroy_route53 destroy_docker_registry
 
 .PHONY: global_env destroy_global_env
