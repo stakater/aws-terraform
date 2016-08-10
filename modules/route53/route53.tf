2af45dd750c20d65fa98613989cda647fc0a9f8e
hazim1093 <hazim_malik@hotmail.com>
2016-08-10 19:09:07 +0500

    Remove docker registry entry from route53 module files

--- modules/route53/route53.tf
+++ modules/route53/route53.tf
@@ -1,7 +1,6 @@
 variable "vpc_id" { }
 #variable "public_domain" { default="dockerage.com" }
 variable "private_domain" { }
-variable "docker_registry_private_ip" { }
 
 resource "aws_route53_zone" "private" {
     name = "${var.private_domain}"
@@ -10,14 +9,6 @@ resource "aws_route53_zone" "private" {
     tags {
         Name = "${var.private_domain}"
     }
-}
-
-resource "aws_route53_record" "registry_local" {
-  zone_id = "${aws_route53_zone.private.zone_id}"
-  name = "registry"
-  type = "A"
-  ttl = "5"
-  records = ["${var.docker_registry_private_ip}"]
 }
 
 output "private_zone_id" {value = "${aws_route53_zone.private.zone_id}" }
--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,3 +1,2 @@
 variable "vpc_id" { }
 variable "private_domain" { }
-variable "docker_registry_private_ip" { }
