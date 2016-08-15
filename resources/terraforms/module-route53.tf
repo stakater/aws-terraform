e577abe5da2ffaa066612b15fbde4c5494262195
hazim1093 <hazim_malik@hotmail.com>
2016-08-15 13:19:55 +0500

    Add Public domain

--- resources/terraforms/module-route53.tf
+++ resources/terraforms/module-route53.tf
@@ -1,6 +1,6 @@
 module "route53" {
     source = "../modules/route53"
     vpc_id="${module.vpc.vpc_id}"
-    #public_domain = "dockerage.com"
+    public_domain = "${var.public_domain}"
     private_domain = "${var.private_domain}"
 }
